2eac976e4a90ae25f79a3fa05987cfd3bdb96e9a
Bolin <bolinzhao@yahoo.com>
2018-12-12 10:55:26 +0800

    update local code ti Git: finish the EKF part

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,8 @@
 #include "kalman_filter.h"
+#include "tools.h"
+#include <iostream>
 
+using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -9,6 +12,7 @@ using Eigen::VectorXd;
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
+
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
@@ -21,22 +25,67 @@ void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+  std::cout << "Predict Start!" << std::endl;
+	x_ = F_ * x_;
+	MatrixXd Ft = F_.transpose();
+	P_ = F_ * P_ * Ft + Q_;  
+  std::cout << "Predict Updated!" << std::endl;
+  
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+   std::cout << "Laser Start!" << std::endl;
+	VectorXd z_pred = H_ * x_;
+	VectorXd y = z - z_pred;
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_* Ht * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+  std::cout << "Laser Updated!" << std::endl;
 }
 
+
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+  std::cout << "Radar Start!" << std::endl;
+  
+  float rho_2 = x_(0)*x_(0) + x_(1)*x_(1); //Prediction position in normal format
+  if (fabs(rho_2)<0.0001)
+  {
+   rho_2 = 0.0001;
+  }
+  float rho = sqrt(rho_2);
+  float phi = atan2(x_(1), x_(0)); 
+  float rho_dot =  (x_(0)*x_(2) + x_(1)*x_(3))/rho;
+  
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+  
+	VectorXd y = z - z_pred;
+  //Limited the output range into [-pi,pi]
+  y(1) = fmod(y(1),2*pi); 
+ if (y(1) >= pi && y(1) <=2*pi)
+  {
+  y(1) = y(1) -2*pi;
+  }
+  if (y(1) <= -pi && y(1) >=-2*pi)
+ {
+  y(1) = y(1) +2*pi;
+  }
+	MatrixXd Ht = H_.transpose();
+	MatrixXd S = H_ * P_ * Ht + R_;
+	MatrixXd Si = S.inverse();
+	MatrixXd K = P_* Ht * Si;
+
+	//new estimate
+	x_ = x_ + (K * y);
+	long x_size = x_.size();
+	MatrixXd I = MatrixXd::Identity(x_size, x_size);
+	P_ = (I - K * H_) * P_;
+  std::cout << "Radar Updated!" << std::endl;
 }
